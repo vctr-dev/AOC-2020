cb40a0339fccb07236adef3bfa0af006479f8203
Victor Chan <vchan@atlassian.com>
2020-12-02 08:04:19 +1100

    Optimize day 1 puzzle with sort and early break

--- day1/index.js
+++ day1/index.js
@@ -2,13 +2,19 @@ const fs = require("fs");
 const numbers = fs
 	.readFileSync("./numbers.txt", { encoding: "utf-8" })
 	.split("\n")
-	.map((n) => +n);
+	.map((n) => +n)
+	.sort((a, b) => a - b);
 
+const TARGET = 2020;
 function getPair(nums) {
-	for (let i = 0; i < nums.length; i++) {
-		for (let j = i + 1; j < nums.length; j++) {
+	for (let i = 0; i < nums.length - 2; i++) {
+		for (let j = i + 1; j < nums.length - 1; j++) {
+			const pairTotal = nums[i] + nums[j];
+			if (pairTotal > TARGET) break;
 			for (let k = j + 1; k < nums.length; k++) {
-				if (nums[i] + nums[j] + nums[k] === 2020) {
+				const total = pairTotal + nums[k];
+				if (total > TARGET) break;
+				if (total === TARGET) {
 					return nums[i] * nums[j] * nums[k];
 				}
 			}
