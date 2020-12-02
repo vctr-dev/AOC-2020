fea1ffb72e26373039e632a4037299db2b1ac572
Victor Chan <vchan@atlassian.com>
2020-12-02 16:33:51 +1100

    Fix linting issues

--- day2/index.js
+++ day2/index.js
@@ -12,13 +12,16 @@ const inputs = fs
 
 console.log(inputs.length);
 
+// eslint-disable-next-line no-unused-vars
 function passwordCheck1({ policy, password }) {
+	// eslint-disable-next-line no-unused-vars
 	const [_, min, max, letter] = /(\d+)-(\d+) (\w)/.exec(policy);
 	const num = password.split("").filter((c) => c === letter).length;
 	return num <= max && num >= min;
 }
 
 function passwordCheck2({ policy, password }) {
+	// eslint-disable-next-line no-unused-vars
 	const [_, nPos, letter] = /(\d+-\d+) (\w)/.exec(policy);
 	const pairs = nPos.split("-").map((n) => password[n - 1]);
 	return pairs.filter((c) => c === letter).length === 1;
