f8f086f2625f59699af6edff3c0e689e2efcb2cc
Victor Chan <vchan@atlassian.com>
2020-12-04 16:35:25 +1100

    Initial solution for day 4

---
+++ day4/index.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const path = require("path");
+
+const between = (a, b) => (v) => v >= a && v <= b;
+
+const requiredFields = [
+	{ key: "byr", test: between(1920, 2002) },
+	{ key: "iyr", test: between(2010, 2020) },
+	{ key: "eyr", test: between(2020, 2030) },
+	{
+		key: "hgt",
+		test: (v) => {
+			const [match, num, unit] = /^(\d*)(cm|in)$/.exec(v) || [];
+			if (match) {
+				if (unit === "cm") return between(150, 193)(num);
+				if (unit === "in") return between(59, 76)(num);
+			}
+		},
+	},
+	{ key: "hcl", test: (v) => /^#[0-9a-f]{6}$/.test(v) },
+	{
+		key: "ecl",
+		test: (v) => ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(v),
+	},
+	{ key: "pid", test: (v) => /^\d{9}$/.test(v) },
+];
+
+function getInput() {
+	return fs
+		.readFileSync(path.join(__dirname, "./input.txt"), { encoding: "utf-8" })
+		.split("\n\n")
+		.map((line) =>
+			line
+				.replace(/\n/g, " ")
+				.split(" ")
+				.reduce((acc, v) => {
+					const [match, key, value] = /^(.+):(.+)$/.exec(v);
+					return { ...acc, [key]: value };
+				}, {})
+		);
+}
+
+function solution() {
+	const input = getInput();
+	return input.filter((passport) => {
+		return requiredFields.every(({ key, test }) => {
+			if (!passport[key]) return;
+			if (!test(passport[key]))
+				console.log(key, passport[key], test(passport[key]));
+			return passport[key] && test(passport[key]);
+		});
+	}).length;
+}
+
+console.log(solution());
