f44530d571ce149f19264c15e48f7bedc44b88dd
Victor Chan <vchan@atlassian.com>
2020-12-07 11:18:46 +1100

    Add more setup logic

--- setup-day.sh
+++ setup-day.sh
@@ -3,3 +3,9 @@
 inputFileUrl="https://adventofcode.com/2020/day/$1/input"
 outputFile="./day$1/input.txt"
 curl "$inputFileUrl" --output "$outputFile" --cookie "$(cat ./.cookie)"
+
+inputFileUrl="https://adventofcode.com/2020/day/$1"
+outputFile="./day$1/problem.html"
+curl "$inputFileUrl" --output "$outputFile" --cookie "$(cat ./.cookie)"
+google-chrome "$outputFile" &
+nodemon "./day$1/index.js"
