145432a1439ad5a0afc5d5a3762afab5896b1c2f
Victor Chan <victor.z.chan@gmail.com>
2020-12-13 18:29:50 +1100

    Day 13 initial solution

---
+++ day13/index.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const path = require("path");
+
+const file = process.env.RUN_SAMPLE ? "sample.txt" : "input.txt";
+function parseInput() {
+	const input = fs
+		.readFileSync(path.join(__dirname, file), { encoding: "utf-8" })
+		.split("\n");
+	const [ts, b] = input;
+	return {
+		ts: +ts,
+		b: b.split(",").map((c) => (isNaN(+c) ? c : +c)),
+	};
+}
+
+function partOne({ ts, b }) {
+	b = b.filter((c) => c !== "x");
+	let indexOfSmallest;
+	let smallest = Infinity;
+	b.map((b1) => b1 - (ts % b1)).forEach((b1, i) => {
+		if (b1 < smallest) {
+			indexOfSmallest = i;
+			smallest = b1;
+		}
+	});
+	const n = b[indexOfSmallest];
+
+	return smallest * n;
+}
+
+function partTwo({ b }, prevAns) {
+	b = b
+		.map((b1, i) => {
+			if (b1 === "x") return;
+			return { n: b1, o: i };
+		})
+		.filter(Boolean)
+		.sort((a, b) => -a.n + b.n);
+	let ts = b[0].n - b[0].o;
+	let currentLcm = b[0].n;
+	for (let i = 1; i < b.length; i++) {
+		let b1 = b[i];
+		while ((ts + b1.o) % b1.n !== 0) {
+			ts += currentLcm;
+		}
+		currentLcm *= b1.n;
+	}
+	return ts;
+}
+
+console.log(solution());
+
+function solution() {
+	const input = parseInput();
+	const partOneAns = partOne(input);
+
+	return { partOne: partOneAns, partTwo: partTwo(input, partOneAns) };
+}
